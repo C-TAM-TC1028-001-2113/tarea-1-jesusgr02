c0c27293abd0a45bb6c356bc4c87fe934223ee38
jesusgr02 <89592108+jesusgr02@users.noreply.github.com>
2021-08-30 10:18:20 -0500

    Add files via upload

--- assignments/10Videojuego/src/exercise.py
+++ assignments/10Videojuego/src/exercise.py
@@ -1,6 +1,11 @@
 def main():
     # escribe tu código abajo de esta línea
-    pass
+    JuegosNuevos = float(input("Dame el numero de juegos nuevos que te llevaras: "))
+    JuegosViejos = float(input("Dame el numero de juegos viejos que te llevaras: "))
+    CostoNuevos = JuegosNuevos*1000
+    CostoViejos = JuegosViejos*350
+    CostoTotal = CostoNuevos + CostoViejos
+    print("El cosot total a pagar es:", CostoTotal)
 
 
 if __name__ == '__main__':
