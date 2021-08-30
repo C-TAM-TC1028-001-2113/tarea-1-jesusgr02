ca27a05350b9cb94797415fcb335614697187e97
jesusgr02 <89592108+jesusgr02@users.noreply.github.com>
2021-08-30 10:20:17 -0500

    Add files via upload

--- assignments/19Editorial/src/exercise.py
+++ assignments/19Editorial/src/exercise.py
@@ -1,7 +1,17 @@
 def main():
     # escribe tu código abajo de esta línea
-    pass
+    Paginas = float(input("Dame el numero de paginas que quieres publicar: "))
+    NumeroPalabras = float(input("Indica cual es el numero de palabras que quieres incluir en tu publicacion: "))
 
+    Coste = NumeroPalabras * 60
+    preciototal = Coste - (Coste * 0.10)
+
+    if NumeroPalabras >= 475:
+        print('Podemos considerar la pagina')
+    else:
+        print('Estatus: Pagina no a considerar')
+
+    print('El total de coste por pagina en tu publicacion es:', preciototal)
 
 if __name__ == '__main__':
     main()
