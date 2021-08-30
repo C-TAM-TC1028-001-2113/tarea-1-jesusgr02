c4987e382f2ac4f17092fb27b1ae7cfc9b9fc395
jesusgr02 <89592108+jesusgr02@users.noreply.github.com>
2021-08-30 10:19:21 -0500

    Add files via upload

--- assignments/18CuentaBancaria/src/exercise.py
+++ assignments/18CuentaBancaria/src/exercise.py
@@ -1,7 +1,14 @@
 def main():
     # escribe tu código abajo de esta línea
-    pass
+    SaldoAnterior = float(input("Dame la cantidad de los ingresos mensuales anteriores: "))
+    Ingresos = float(input("Dame la cantidad de ingresos: "))
+    Egresos = float(input("Dame el valor de los egresos: "))
+    Cheque = float(input("Dame el la cantidad de cheques expedidos: "))
 
+    Total = (SaldoAnterior + Ingresos) - (Egresos + (Cheque*13))
+    SaldoTotal = Total - (Total*0.075)
 
+    print("El saldo total que tienes es:", SaldoTotal)
+    
 if __name__ == '__main__':
     main()
