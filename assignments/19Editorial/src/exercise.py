ad2b1cc725baae6d00c5680c2bbb0998753d6458
jesusgr02 <89592108+jesusgr02@users.noreply.github.com>
2021-09-08 17:40:34 -0500

    Ejercicio Editorial Jesus Guerrero
    Formato GitHub

--- assignments/19Editorial/src/exercise.py
+++ assignments/19Editorial/src/exercise.py
@@ -1,17 +1,15 @@
 def main():
     # escribe tu código abajo de esta línea
-    Paginas = float(input("Dame el numero de paginas que quieres publicar: "))
-    NumeroPalabras = float(input("Indica cual es el numero de palabras que quieres incluir en tu publicacion: "))
+    NumeroPalabras = int(input("Dame el número de palabras: "))
+    Parte1 = NumeroPalabras/475
+    Parte2 = int(Parte1)
+    if Parte1 - Parte2 > 0:
+        Parte2 = Parte2 + 1
 
-    Coste = NumeroPalabras * 60
-    preciototal = Coste - (Coste * 0.10)
-
-    if NumeroPalabras >= 475:
-        print('Podemos considerar la pagina')
-    else:
-        print('Estatus: Pagina no a considerar')
-
-    print('El total de coste por pagina en tu publicacion es:', preciototal)
+    Precio = Parte2 * 60
+    PrecioTotal = Precio - (Precio * 0.10)
+    print("El costo de la publicación es: ", PrecioTotal)
+    
 
 if __name__ == '__main__':
     main()
